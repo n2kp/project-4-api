7bd891991761dd104df02153539bec0398b62d7c
James Clarke <jamesraymondclarke@gmail.com>
2017-07-21 14:10:49 +0100

    seeds update, passwor_digest

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,15 @@
   ActiveRecord::Base.connection.execute("TRUNCATE #{model.table_name} RESTART IDENTITY CASCADE")
 end
 
-simon = User.create!(username: "simona",  firstname:"Simon", lastname:"Amor", email: "simon@justo.com", password_digest: "password", image:"simon.jpeg", is_dev: true, tech_stack:"HTML, CSS, JS, Angular")
+ User.create!(username: "simona",  firstname:"Simon", lastname:"Amor", email: "simon@justo.com", password: "password", password_confirmation: "password", image:"simon.jpeg", is_dev: true, tech_stack:"HTML, CSS, JS, Angular")
 
-tim = User.create!(username: "timr",  firstname:"Tim", lastname:"Rooke", email: "tim@justo.com", password_digest: "password", image:"tim.jpeg", is_dev:true, tech_stack:"HTML, CSS, JS, Angular")
+ User.create!(username: "timr",  firstname:"Tim", lastname:"Rooke", email: "tim@justo.com", password: "password",password_confirmation: "password", image:"tim.jpeg", is_dev:true, tech_stack:"HTML, CSS, JS, Angular")
 
-nimesh = User.create!(username: "nimeshp",  firstname:"Nimesh", lastname:"Patel", email: "nimesh@justo.com", password_digest: "password", image:"nimesh.jpeg", is_dev:false)
+ User.create!(username: "nimeshp",  firstname:"Nimesh", lastname:"Patel", email: "nimesh@justo.com", password: "password",password_confirmation: "password", image:"nimesh.jpeg", is_dev:false)
 
-james = User.create!(username: "jamesc",  firstname:"James", lastname:"Clarke", email: "james@justo.com", password_digest: "password", image:"james.jpeg", is_dev:false)
+ User.create!(username: "jamesc",  firstname:"James", lastname:"Clarke", email: "james@justo.com", password: "password",password_confirmation: "password", image:"james.jpeg", is_dev:false)
 
-website = Project.create!(
+Project.create!(
 
   title:"Floral Ecommerce website needed ASAP",
   description:"I need a responsive commerce website to be build asap. I have some flowers and stuff that I really need to sell online because people need an online based flower experience to rival that of apple, uber and airB&B. Please help me!",
