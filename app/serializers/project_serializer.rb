b20a17f7bea379054ae737446d2ae880cf0c4a8c
James Clarke <jamesraymondclarke@gmail.com>
2017-07-22 15:49:32 +0100

    resolving user issues

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,5 @@
 class ProjectSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :project_deadline, :bid_deadline, :tech_stack, :budget, :is_active, :is_priority
+  attributes :id, :title, :description, :project_deadline, :bid_deadline, :tech_stack, :budget, :is_active, :user, :is_priority
   has_one :user
   has_many :tenders
 
