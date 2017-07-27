6f09e148aaed854fd3a32ccffbd781d1901be577
Nimesh Patel <n2kp@hotmail.com>
2017-07-27 09:51:53 +0100

    App rename complete

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins ['http://localhost:7000', 'https://mighty-waters-44264.herokuapp.com']
+    origins ['http://localhost:7000', 'https://justo-app.herokuapp.com']
 
     resource '*',
       headers: :any,
